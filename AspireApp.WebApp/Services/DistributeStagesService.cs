3a48335168fd0ee687dc73f2c6b35d90adfac08b
SigridAndersen <sigrid.0489@gmail.com>
2025-04-09 18:19:51 +0200

    Create DistributeStagesService.cs

---
+++ AspireApp.WebApp/Services/DistributeStagesService.cs
@@ -0,0 +1,61 @@
+﻿using AspireApp.WebApp.Database;
+
+namespace AspireApp.WebApp.Services;
+
+public class DistributeStagesService
+{
+    public List<Position> Distribute(List<Candidate> candidates)
+    {
+        const int totalStages = 15;
+
+        var assignedStages = new HashSet<int>();
+
+        var positions = new List<Position>();
+
+        int positionId = 1;
+
+        foreach (var candidate in candidates)
+        {
+            var assignedStage = candidate.PreferredStages
+                .FirstOrDefault(stage => stage <= totalStages && !assignedStages.Contains(stage));
+
+            if (assignedStage != 0)
+            {
+                positions.Add(new Position
+                {
+                    Id = positionId++,
+                    Name = candidate,
+                    Stage = assignedStage
+                });
+                assignedStages.Add(assignedStage);
+            }
+            else
+            {
+                var unassignedStage = Enumerable.Range(1, totalStages)
+                    .FirstOrDefault(stage => !assignedStages.Contains(stage));
+
+                if (unassignedStage == 0)
+                {
+                    positions.Add(new Position
+                    {
+                        Id = positionId++,
+                        Name = candidate,
+                        Stage = -1
+                    });
+                }
+                else
+                {
+                    positions.Add(new Position
+                    {
+                        Id = positionId++,
+                        Name = candidate,
+                        Stage = unassignedStage
+                    });
+                    assignedStages.Add(unassignedStage);
+                }
+            }
+        }
+
+        return positions;
+    }
+}
